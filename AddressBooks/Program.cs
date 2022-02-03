117d3e4de72e1f7ae44f5b1c31f599d870f79340
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 19:48:44 +0530

    SortedList

--- AddressBooks/Program.cs
+++ AddressBooks/Program.cs
@@ -1,167 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Collections
 {
-    class Program
+    class SortedList
     {
-
-        public static Dictionary<string, List<AddrBook>> addressBook = new Dictionary<string, List<AddrBook>>();
-        public static Dictionary<string, List<AddrBook>> City = new Dictionary<string, List<AddrBook>>();
-        public static Dictionary<string, List<AddrBook>> State = new Dictionary<string, List<AddrBook>>();
-        static void Main(string[] args)
+        //Adding values to sorted list
+        public static void SortedListDemo()
         {
-
-            Console.WriteLine("Welcome to Address Book System!");
-            Console.WriteLine("Enter the number of address books: ");
-            int noOfAddressBook = Convert.ToInt32(Console.ReadLine());
-            int noOfBooks = 0;
-            while (noOfBooks < noOfAddressBook)
-            {
-                Console.WriteLine("Enter the address book name : ");
-                string addressbookname = Console.ReadLine();
-                AddrBook addrBook = new AddrBook();
-                Console.WriteLine("Enter the no of contacts in the address book: ");
-                int noOfContact = Convert.ToInt32(Console.ReadLine());
-
-                while (noOfContact > 0)
-                {
-                    Console.WriteLine("Enter the details of contact to be added: ");
-
-                    Console.Write("Enter First Name: ");
-                    string FirstName = Console.ReadLine();
-
-                    Console.Write("Enter Last Name: ");
-                    string LastName = Console.ReadLine();
-
-                    Console.Write("Enter Phone Number: ");
-                    string PhoneNumber = Console.ReadLine();
-
-                    Console.Write("Enter Address : ");
-                    string Addresses = Console.ReadLine();
-
-                    Console.Write("Enter City : ");
-                    string City = Console.ReadLine();
-
-                    Console.Write("Enter State : ");
-                    string State = Console.ReadLine();
-
-                    Console.Write("Enter ZipCode: ");
-                    string ZipCode = Console.ReadLine();
-
-                    Console.Write("Enter EmailId: ");
-                    string EmailId = Console.ReadLine();
-                    addrBook.GetCustomer(FirstName, LastName, PhoneNumber, Addresses, City, State, ZipCode, EmailId);
-                    noOfContact--;
-                    Console.WriteLine(" ");
-                    addrBook.ListingPeople();
-                }
-                Console.WriteLine("1.To modify the details");
-                Console.WriteLine("2.To remove the details");
-                Console.WriteLine("3.city or state");
-                Console.WriteLine("3.Exit!"); int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        List.ListDemo();
-                        break;
-                    case 2:
-                        SortedList.SortedListDemo();
-                        break;
-                    case 3:
-                        Dictionary.DictionaryDemo();
-                        break;
-                    case 4:
-                
-                        Collections.AddressBook.GetCustomer();
-                        Collections.AddressBook.ListingPeople();
-                        break;
-                    case 5:
-                        Collections.AddressBook.GetCustomer();
-                        AddressBook.Modify();
-                        Collections.AddressBook.ListingPeople();
-                        break;
-                    
-                    case 6:
-
-                        Collections.AddressBook.GetCustomer();
-                        Collections.AddressBook.RemovePeople();
-                        break;
-                    case 7:
-                        Console.WriteLine("Enter 1-To Search a person through a City");
-                        Console.WriteLine("Enter 2-To Search a person through a State");
-                        Console.WriteLine("Enter 3-To view a person by state list or city list");
-                        int opt = Convert.ToInt32(Console.ReadLine());
-                        switch (opt)
-                        {
-
-                            case 1:
-                                SearchAddress(opt);
-                                break;
-                            case 2:
-                                SearchAddress(opt);
-                                break;
-                            case 3:
-                                AddrBook.DisplayCityorState();
-
-                                break;
-                            default:
-                                Console.WriteLine("Invalid Option!");
-                                break;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Enter a valid option");
-                        break;
+            SortedList<int, string> numberNames = new SortedList<int, string>();
+            numberNames.Add(3, "Three");
+            numberNames.Add(1, "One");
+            numberNames.Add(2, "Two");
+            numberNames.Add(4, null);
+            numberNames.Add(10, "Ten");
+            numberNames.Add(5, "Five");
+            Console.WriteLine("SortedList are ", numberNames);
 
 
-                }
-                if (addressBook.ContainsKey(addressbookname))
-                {
-                    Console.WriteLine("Existing address book name");
-                    return;
-                }
-                else
-                {
-                    addressBook.Add(addressbookname, addrBook.people);
-                }
+            //sorted list default sorting order
+            SortedList<int, string> numberName = new SortedList<int, string>()
+                                    {
+                                        {3, "Three"},
+                                        {5, "Five"},
+                                        {1, "One"}
+                                    };
 
+            Console.WriteLine("---Initial key-values--");
 
-                noOfBooks++;
-            }
+            foreach (KeyValuePair<int, string> kvp in numberName)
+                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
 
-            foreach (KeyValuePair<string, List<AddrBook>> addr in addressBook)
-            {
-                Console.WriteLine("The address Books are:{0}", addr.Key);
+            numberName.Add(6, "Six");
+            numberName.Add(2, "Two");
+            numberName.Add(4, "Four");
 
-            }
+            Console.WriteLine("---After adding new key-values--");
+
+            foreach (var kvp in numberName)
+                Console.WriteLine("key: {0}, value: {1}", kvp.Key, kvp.Value);
+
 
         }
-        static void SearchAddress(int option)
-        {
-            string city, state;
-            if (option == 1)
-            {
-                Console.WriteLine("Enter the City Name");
-                city = Console.ReadLine();
-                foreach (KeyValuePair<string, List<AddrBook>> kvp in addressBook)
-                {
-                    AddrBook.StoreCityList(kvp.Key, kvp.Value, city);
-
-                }
-                if (option == 2)
-                {
-                    Console.WriteLine("Enter the City Name");
-                    state = Console.ReadLine();
-                    foreach (KeyValuePair<string, List<AddrBook>> kvp in addressBook)
-                    {
-                        AddrBook.StoreStateList(kvp.Key, kvp.Value, state);
-                    }
-                }
-            }
-        }
-
-
     }
-    
-}
+}   
