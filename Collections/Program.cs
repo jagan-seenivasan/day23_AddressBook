1a88d9f4dc768318b43bbb562a69405215639fa6
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 20:26:06 +0530

    Multiple Contacts

---
+++ Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collections Demo Problem!!!");
+            Console.WriteLine("Enter the option ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    List.ListDemo();
+                    break;
+                case "2":
+                    SortedList.SortedListDemo();
+                    break;
+                case "3":
+                    Dictionary.DictionaryDemo();
+                    break;
+                case "4":
+                
+                    Collections.AddressBook.GetCustomer();
+                    Collections.AddressBook.Modify();
+                    Collections.AddressBook.ListingPeople();
+                    Collections.AddressBook.RemovePeople();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
