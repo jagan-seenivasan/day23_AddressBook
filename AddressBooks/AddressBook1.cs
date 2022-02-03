f117bcb4ee6a66a3fb0deba707323480b50a34c7
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 19:43:30 +0530

    Using Dictionary

---
+++ AddressBooks/AddressBook1.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Collections
+{
+    internal class AddressBook1
+    {
+        internal static void GetCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void ListingPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Modify()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void RemovePeople()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
